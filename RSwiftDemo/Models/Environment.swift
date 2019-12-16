81b216173b51b8ccec28e9a3f41bccc1e9c1dde5
Jackson Chung <jackson.chung@gogotech.hk>
2019-12-16 16:00:50 +0800

    add config for phraseapp

---
+++ RSwiftDemo/Models/Environment.swift
@@ -0,0 +1,39 @@
+//
+//  Environment.swift
+//  RSwiftDemo
+//
+//  Created by Jackson Chung on 16/12/2019.
+//  Copyright © 2019 Jackson Chung. All rights reserved.
+//
+
+import Foundation
+
+//
+//  Environment.swift
+//  GoGoVanClientV2
+//
+//  Created by Jackson Chung on 6/11/2018.
+//  Copyright © 2018 GoGo Tech Limited. All rights reserved.
+//
+
+import UIKit
+
+import Foundation
+
+enum PlistKey: String {
+    case phraseaAppEnvToken = "PhraseApp Environment Token"
+    case phraseAppDistributionId = "PhraseApp Distribution ID"
+}
+
+struct Environment {
+    fileprivate static func infoDict(keyName: String) -> Any? {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("Plist file not found")
+        }
+        return dict[keyName]
+    }
+
+    static func configuration(keyName: PlistKey) -> String {
+        return infoDict(keyName: keyName.rawValue) as? String ?? ""
+    }
+}
